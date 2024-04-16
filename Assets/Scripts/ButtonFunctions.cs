e846c6439902b3f5b65453c1012b44472b5223b6
Gump0 <blenderjack2004@gmail.com>
2024-04-15 21:03:40 -0400

    fixed input bug, and begun setting up arrays

--- Assets/Scripts/ButtonFunctions.cs
+++ Assets/Scripts/ButtonFunctions.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ButtonFunctions : MonoBehaviour
 {
-    public void CallMethod(string methodName)
-    {
+    public void CallMethod(string methodName){
         Invoke(methodName, Time.deltaTime); //Call this without MONOBEHAVIOR
     }
-
-    public void Test()
-    {
-        Debug.Log("Test");
+    public void Test1(){
+        Debug.Log("Left");
+    }
+    public void Test2(){
+        Debug.Log("Right");
+    }
+    public void Test3(){
+        Debug.Log("Up");
+    }
+    public void Test4(){
+        Debug.Log("Down");
     }
 }
--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -11,18 +11,34 @@ public class ButtonManager : MonoBehaviour
 
     private GameObject buttonHighlight;
     private UIInputManager inputManager;
-    private Image[] listOfButtons;
+    [SerializeField] private Image[] listOfButtons;
+    public int buttonIndex = 0; //switch to private after
 
     public string[] methods;
     private void Awake()
     {
         inputManager = GetComponent<UIInputManager>();  
         buttonFunctions = GetComponent<ButtonFunctions>();
+        if (buttonHighlight == null) buttonHighlight = GameObject.Find("Highlight");
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(inputManager.buttonKeys[0])) buttonFunctions.CallMethod(methods[0]);
-        if (Input.GetKeyDown(inputManager.buttonKeys[1])) buttonFunctions.CallMethod(methods[1]);
+        if (Input.GetKeyDown(inputManager.buttonKeys[0])){ // Left Input
+            buttonFunctions.CallMethod(methods[0]);
+            if(buttonIndex >= listOfButtons.Length +1) buttonIndex -=2;
+        }
+        if (Input.GetKeyDown(inputManager.buttonKeys[1])){ // Right Input
+            buttonFunctions.CallMethod(methods[1]);
+            if(buttonIndex <= listOfButtons.Length -1) buttonIndex +=2;
+        }
+        if (Input.GetKeyDown(inputManager.buttonKeys[2])){ // Up Input
+            buttonFunctions.CallMethod(methods[2]);
+            if(buttonIndex != listOfButtons.Length) buttonIndex++;
+        }
+        if (Input.GetKeyDown(inputManager.buttonKeys[3])){ // Down Input
+            buttonFunctions.CallMethod(methods[3]); 
+            if(buttonIndex != 0) buttonIndex--;
+        }
     }
 }
